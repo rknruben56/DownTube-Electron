5084043283a749d66628beaa2960b915fd8117ea
Ruben <rrodriguez6@outlook.com>
2020-03-25 18:11:17 -0500

    added spinners and cleaned up packages

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,32 +2,31 @@ import * as React from 'react';
 import { useForm, ErrorMessage } from 'react-hook-form';
 import { homedir } from 'os';
 import { youtubeService } from '../services/youtube-service';
-import LoadingOverlay from 'react-loading-overlay';
 import { AppForm } from './app-form';
 import { DownloadParams } from '../services/download-params';
-import { Button } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 
 const defaultDirectory = `${homedir}\\Youtube`;
 const defaultTitle = 'Download';
 const requiredMessage = 'This field is required.';
 
 const urlValidation = {
-  required: requiredMessage, 
-  pattern: { 
-    value: /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\//, 
+  required: requiredMessage,
+  pattern: {
+    value: /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\//,
     message: 'Please enter a valid YouTube URL.'
-  } 
+  }
 };
 
 const App = () => {
-  const [isActive, setActive] = React.useState(false);
-  const [loadingText, setLoadingText] = React.useState('');
+  const [isLoading, setLoading] = React.useState(false);
   const [isSuccess, setSuccess] = React.useState(false);
   const [isError, setError] = React.useState(false);
+  const [isDownloading, setDownloading] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState('');
 
   const { register, handleSubmit, errors, getValues, setValue, clearError, triggerValidation } = useForm();
-  
+
   const onSubmit = (data: AppForm) => {
     let directory = data.directory || defaultDirectory;
     let title = data.title || defaultTitle;
@@ -40,7 +39,7 @@ const App = () => {
       onError: onError
     };
 
-    startLoading('Downloading video');
+    setDownloading(true);
     youtubeService.download(downloadParams);
   };
 
@@ -56,64 +55,57 @@ const App = () => {
     let result = await triggerValidation('url');
     if (result) {
       clearError('url');
-      startLoading('Loading title');
+      setLoading(true);
+
       youtubeService.getTitle(url)
         .then(title => {
+          setLoading(false);
           setValue('title', title);
-          stopLoading();
         });
     }
   };
 
-  const startLoading = (text: string) => {
-    setActive(true);
-    setLoadingText(text);
-  };
-
-  const stopLoading = () => {
-    setActive(false);
-    setLoadingText('');
-  }
-
   const onError = (output: string) => {
-    stopLoading();
+    setDownloading(false);
     setError(true);
     setErrorMessage(output);
   }
 
   const onDownloadComplete = () => {
-    stopLoading();
+    setDownloading(false);
     setSuccess(true);
   };
 
   return (
-    <LoadingOverlay active={isActive} spinner text={loadingText}>
-      <div className='app container-fluid'>
-        <h1>DownTube</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <label htmlFor="url">YouTube URL</label>
-            <input id="url" name="url" className="form-control" ref={register(urlValidation)} onBlur={onUrlBlur} />
-            <ErrorMessage errors={errors} name="url" as="span"/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <input id="title" name="title" className="form-control" ref={register} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="directory">Folder</label>
-            <input id="directory" className="form-control" name="directory" defaultValue={defaultDirectory} ref={register({ required: requiredMessage })} />
-            <ErrorMessage errors={errors} name="directory" as="span"/>
-            <Button variant="secondary" type="button" size="lg" block>Select Folder</Button>
-          </div>
-          <div className="submit">
-            <Button variant="primary" type="submit" size="lg" block>Download</Button>
-          </div>
-        </form>
-        {isError && <div className="alert alert-danger" role="alert">{errorMessage}</div>}
-        {isSuccess && <div className="alert alert-success" role="alert">Video Successfully downloaded!</div>}
-      </div>
-    </LoadingOverlay>
+    <div className='app container-fluid'>
+      <h1>DownTube</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="url">YouTube URL</label>
+          <input id="url" name="url" className="form-control" ref={register(urlValidation)} onBlur={onUrlBlur} />
+          <ErrorMessage errors={errors} name="url" as="p" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          {isLoading && <div className="spinner"><Spinner className="spinner" animation="border" as="span" /></div>}
+          {!isLoading && <input id="title" name="title" className="form-control" ref={register} />}
+        </div>
+        <div className="form-group">
+          <label htmlFor="directory">Folder</label>
+          <input id="directory" className="form-control" name="directory" defaultValue={defaultDirectory} ref={register({ required: requiredMessage })} />
+          <ErrorMessage errors={errors} name="directory" as="p" />
+          <Button variant="secondary" type="button" size="lg" block>Select Folder</Button>
+        </div>
+        <div className="submit">
+          <Button variant="primary" type="submit" size="lg" block disabled={isDownloading}>
+            {!isDownloading && <span>Download</span>}
+            {isDownloading && <><Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true"/> Downloading...</>}
+          </Button>
+        </div>
+      </form>
+      {isError && <div className="alert alert-danger" role="alert">{errorMessage}</div>}
+      {isSuccess && <div className="alert alert-success" role="alert">Video Successfully downloaded!</div>}
+    </div>
   )
 }
 export default App;
