78ae54b9482ac221b5fc6e7c819e6909b4cfdd2d
Ruben <rrodriguez6@outlook.com>
2020-03-25 09:41:20 -0500

    Added react-loading-overlay package

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useForm } from 'react-hook-form';
 import { homedir } from 'os';
 import { youtubeControl } from '../youtube/youtube-control';
+import LoadingOverlay from 'react-loading-overlay';
 
 interface AppForm {
   url: string;
@@ -13,9 +14,12 @@ const defaultDirectory = `${homedir}/Youtube`;
 const defaultTitle = 'Download';
 
 const App = () => {
+  const [isActive, setActive] = React.useState(false);
+  const [loadingText, setLoadingText] = React.useState('');
+
   const { register, handleSubmit, errors, getValues, setValue } = useForm();
+  
   const onSubmit = (data: AppForm) => {
-    console.log(data)
     let directory = data.directory || defaultDirectory;
     let title = data.title || defaultTitle;
     youtubeControl.download(data.url, directory, title);
@@ -25,33 +29,49 @@ const App = () => {
     let values = getValues();
     let url = values.url;
     if (url) {
-      // TODO: Add loading spinner
+      startLoading('Loading title');
       youtubeControl.getTitle(url)
         .then(title => {
           setValue('title', title);
+          stopLoading();
         });
     }
   };
 
+  const startLoading = (text: string) => {
+    setActive(true);
+    setLoadingText(text);
+  };
+
+  const stopLoading = () => {
+    setActive(false);
+    setLoadingText('');
+  }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}> 
-      <div className="form-group">
-        <label htmlFor="url">YouTube URL</label>
-        <input id="url" name="url" className="form-control" ref={register({ required: true })} onBlur={onUrlBlur} />
-        {errors.url && <span className="error">This field is required.</span>}
+    <LoadingOverlay active={isActive} spinner text={loadingText}>
+      <div className='app container-fluid'>
+        <h1>DownTube</h1>
+        <form onSubmit={handleSubmit(onSubmit)}> 
+          <div className="form-group">
+            <label htmlFor="url">YouTube URL</label>
+            <input id="url" name="url" className="form-control" ref={register({ required: true })} onBlur={onUrlBlur} />
+            {errors.url && <span className="error">This field is required.</span>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input id="title" name="title" className="form-control" ref={register} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="directory">Folder</label>
+            <button type="button" id="directory" name="directory" className="btn btn-secondary form-control">Select Folder</button>
+          </div>
+          <div className="submit">
+            <button id="download" className="btn btn-primary btn-lg btn-block">Download</button>
+          </div>
+        </form>
       </div>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <input id="title" name="title" className="form-control" ref={register} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="directory">Folder</label>
-        <button type="button" id="directory" name="directory" className="btn btn-secondary form-control">Select Folder</button>
-      </div>
-      <div className="submit">
-        <button id="download" className="btn btn-primary btn-lg btn-block">Download</button>
-      </div>
-    </form>
+    </LoadingOverlay>
   )
 }
 export default App;
--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -3,15 +3,10 @@
  */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as App from '../app/app';
+import App from '../app/app';
 
 // Import the styles here to process them with webpack
 import '@public/style.css';
 
-ReactDOM.render(
-  <div className='app container-fluid'>
-    <h1>DownTube</h1>
-    <App.default />
-  </div>,
-  document.getElementById('app')
+ReactDOM.render(<App />, document.getElementById('app')
 );
